7b53d7eb8992f16606be98e830852992eaea6717
Lucky Kim <lucky.hw.kim@gmail.com>
2022-03-29 22:43:28 +0000

    Add rendertweet and createtweetelement functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,59 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  const renderTweets = function (tweets) {
+    for(let tweet of tweets) {
+      console.log(tweet);
+     $('#tweet-container').append(createTweetElement(tweet))
+    }
+  }
 
+  const createTweetElement = function (tweet) {
+    let $tweet = `<article class="tweet"> 
+            <header>
+              <div class="user">
+                <img class="avatar" src=${tweet.user.avatars}>
+                <p>${tweet.user.name}</p>
+              </div>
+              <div class="user-handle">${tweet.user.handle}</div>
+            </header>
+                <h4 class="tweet-msg">${tweet.content.text}</h4>
+                <div class="tweet-msg-line"></div>
+            <footer>
+              <div class="post-date">${tweet.created_at}</div>
+              <div class="tweet-icons">
+                <i class="fa-solid fa-flag"></i>
+                <i class="fa-solid fa-retweet"></i>
+                <i class="fa-solid fa-heart"></i>
+              </div>
+            </footer>
+          </article>`
+    return $tweet;
+  }
+  renderTweets(data);
+})
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,49 +1,23 @@
 $(document).ready(function() {
-  console.log('loaded')
-});
+  console.log('scripts have been loaded');
 
-document.querySelector("#tweet-text").focus();
-document.querySelector("#tweet-text").setSelectionRange(0,0);
-
-
-// $('#tweet-text').blur(function() {
-//   console.log( "Handler for .blur() called.", this );
-// });
-
-// $('#tweet-text').focus(function() {
-//   console.log( "Handler for .focus() called.", this );
-// });
-
-// $( '#tweet-text' ).keydown(function() {
-//   console.log( "Handler for .keydown() called.", this );
-// });
-
-$('#tweet-text').on('input',function() {
-   let countDown = 140-($(this).val().length - 12)
-   let counter = $(this).parent().children('.below-tweet').children('.counter');
-   if(countDown >= 0) {
-   counter.html(countDown).removeClass("text-red")
-   } else {
-    counter.html(countDown).addClass("text-red")
-   }
+  $('.tweet-text').on('input',function() {
+    let countDown = 140-($(this).val().length - 12)
+    let $counter = $(this).parent().children('.below-tweet').children('.counter');
+    if(countDown >= 0) {
+    $counter.html(countDown).removeClass("text-red")
+    } else {
+    $counter.html(countDown).addClass("text-red")
+    }
+  });
 });
 
 
+//$inputField.val('')
 
 
 
 
 
-// $( '#tweet-text' ).change(function() {
-//   console.log( "Handler for .change() called.", this );
-// });
-
-// $( '#tweet-text' ).on(function() {
-//   console.log( "Handler for .input() called.", this );
-// });
-
-// $( '#tweet-text' ).click(function() {
-//   console.log( "Handler for .input() called.", this );
-// });
 
 
